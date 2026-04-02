064f6a21e83997cce7042ed9b6f71156718c2642
Florian Kunz <Florian.Kunz@Kunz-pro.it>
2026-04-02 22:43:32 +0200

    add add transaction funktionallity, add testing seeds for user and account open TODOS: entry_type, transref and description logic

--- app/controllers/Transactions_controller.rb
+++ app/controllers/Transactions_controller.rb
@@ -3,8 +3,54 @@ class TransactionsController < ApplicationController
   end
 
   def new
+    @transaction = Transaction.new(posting_date: Date.current)
   end
 
   def create
+    @transaction = Transaction.new(transaction_params.merge(
+      account_id: find_account.id,
+      user_id: find_user.id,
+      description: build_mamual_description,
+      entry_type: determine_manual_entry_type,
+      transaction_ref: generate_manual_transaction_ref
+    ))
+
+    p @transaction
+    @transaction.save
+  end
+
+  private
+
+  def find_user
+    User.find(1)
+  end
+
+  def find_account
+        Account.find(1)
+  end
+
+  def build_mamual_description
+    "manuel desc"
+  end
+
+  def determine_manual_entry_type
+    :income
+  end
+
+  def generate_manual_transaction_ref
+    "testref"
+  end
+
+  def transaction_params
+        params.require(:transaction).permit(:payment_reference, :amount, :currency, :counterparty, :posting_date)
   end
 end
+
+
+
+=begin
+  description - bei manuellen Buchungen standarttext - manuelle Buchung von [Firstname + Secondname]
+  entry_type - wird abhängig gesetzt, ob die Buchung positiv oder negativ ist.
+  transaction_ref - wir suchen die letzte ref der buchungen mit description > manuelle Buchung und addieren zum Stringwert 1 dazu
+  Beispiel: ref=mt1 > mt2. mt steht für manual transaction
+=end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.find_or_create_by!(username: "fkunz") do |user|
+  user.first_name = "florian"
+  user.last_name = "kunz"
+end
+
+
+Account.find_or_create_by!(iban: "DE02100100100012345678") do |account|
+  account.bank_name = "Volksbank Rhein-Nahe-Hunsrück eG"
+  account.bic = "PBNKDEFFXXX"
+  account.display_name = "Hauptkonto"
+  account.opening_balance = 500
+  account.user_id = user.id
+end
